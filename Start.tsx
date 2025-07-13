6444eba647e7f3e5ff2cb546124674c3e504bb3b
Nadiia2323 <yariginanm@gmail.com>
2025-07-13 18:36:03 +0200

    style updates

--- Start.tsx
+++ Start.tsx
@@ -4,7 +4,7 @@ export default function Start() {
   return (
     <>
       <div className=" flex flex-col justify-center items-center">
-        <h1>HABITYFY - Your path to productivity</h1>
+        <h1>GrowBit - Your path to productivity</h1>
         <h4>Start today — change your life step by step.</h4>
         <button className=" m-5 w-20 h-10 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
           Start
