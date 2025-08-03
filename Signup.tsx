6d3bea1c73b3f103ff9ee55a5899d9569620ff75
Nadiia2323 <yariginanm@gmail.com>
2025-08-03 19:38:51 +0200

    Rewrite signup component using react-hook-form

--- Signup.tsx
+++ Signup.tsx
@@ -1,107 +1,122 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 type PersonalData = {
   email: string;
   password: string;
   repeatPass: string;
-};
-type Errors = {
-  email: string;
-  password: string;
-  repeatPass: string;
+  terms: boolean;
 };
 
 export default function Signup() {
-  const [personalData, setPersonalData] = useState<PersonalData>({
-    email: "",
-    password: "",
-    repeatPass: "",
-  });
-  console.log("personalData :>> ", personalData);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm<PersonalData>({ mode: "onTouched" });
+  const password = watch("password");
+  const onSubmit = (data: PersonalData) => {
+    console.log("data :>> ", data);
+    reset();
+  };
 
-  const [errors, setErrors] = useState<Errors>({
-    email: "",
-    password: "",
-    repeatPass: "",
-  });
-
-  function handlerChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-
-    if (
-      name === "email" &&
-      value &&
-      !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
-    ) {
-      setErrors((prev) => ({ ...prev, email: "invalid email" }));
-    } else {
-      setErrors((prev) => ({ ...prev, email: "" }));
-    }
-
-    if (name === "password" && value.length < 8) {
-      setErrors((prev) => ({ ...prev, password: "too short" }));
-    } else {
-      setErrors((prev) => ({ ...prev, password: "" }));
-    }
-    if (name === "repeat-password") {
-    }
-    console.log("errors :>> ", errors);
-  }
-
+  const getInputClass = (hasError: boolean) => {
+    return hasError
+      ? "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 appearance-none dark:text-white dark:border-red-500 border-red-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600 peer"
+      : "block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer";
+  };
+  const getLableClass = (hasError: boolean) => {
+    return hasError
+      ? "absolute text-sm text-red-600 dark:text-red-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+      : "absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1";
+  };
   return (
-    <form className="max-w-sm space-y-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm space-y-4">
       <h5 className="text-xl font-medium text-gray-900 dark:text-white">
         Sign up
       </h5>
       <div className="relative">
         <input
-          onChange={handlerChange}
-          name="email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Invalid email",
+            },
+          })}
           type="email"
           id="email"
-          className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className={getInputClass(!!errors.email)}
           placeholder=" "
-          required
         />
-        <label
-          htmlFor="email"
-          className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
+        <label htmlFor="email" className={getLableClass(!!errors.email)}>
           Email
         </label>
+        {errors.email && (
+          <p
+            id="outlined_error_help"
+            className="mt-2 text-xs text-red-600 dark:text-red-400"
+          >
+            <span className="font-medium">{errors.email.message}</span>
+          </p>
+        )}
       </div>
       <div className="relative">
         <input
-          onChange={handlerChange}
-          name="password"
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 character",
+            },
+            pattern: {
+              value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/,
+              message: "Must include uppercase, number & special char",
+            },
+          })}
           type="password"
           id="password"
-          className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          className={getInputClass(!!errors.password)}
           placeholder=" "
           required
         />
-        <label
-          htmlFor="password"
-          className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
+        <label htmlFor="password" className={getLableClass(!!errors.password)}>
           Password
         </label>
+        {errors.password && (
+          <p
+            id="outlined_error_help"
+            className="mt-2 text-xs text-red-600 dark:text-red-400"
+          >
+            <span className="font-medium">{errors.password.message}</span>
+          </p>
+        )}
       </div>
       <div className="relative">
         <input
-          onChange={handlerChange}
-          name="repeat-password"
-          type="repeat-password"
-          id="repeat-password"
-          className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          {...register("repeatPass", {
+            required: "Please repeat the password",
+            validate: (value) => value === password || "passwords do not match",
+          })}
+          type="password"
+          id="repeatPass"
+          className={getInputClass(!!errors.repeatPass)}
           placeholder=" "
-          required
         />
         <label
-          htmlFor="repeat-password"
-          className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+          htmlFor="repeatPass"
+          className={getLableClass(!!errors.repeatPass)}
         >
           Repeat password
         </label>
+        {errors.repeatPass && (
+          <p
+            id="outlined_error_help"
+            className="mt-2 text-xs text-red-600 dark:text-red-400"
+          >
+            <span className="font-medium">{errors.repeatPass.message}</span>
+          </p>
+        )}
       </div>
       <div className="flex items-start mb-5">
         <div className="flex items-center h-5">
@@ -110,7 +125,7 @@ export default function Signup() {
             type="checkbox"
             value=""
             className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            required
+            {...register("terms", { required: "You must accept all terms" })}
           />
         </div>
         <label
@@ -125,6 +140,14 @@ export default function Signup() {
             terms and conditions
           </a>
         </label>
+        {errors.terms && (
+          <p
+            id="outlined_error_help"
+            className="mt-2 text-xs text-red-600 dark:text-red-400"
+          >
+            <span className="font-medium">{errors.terms.message}</span>
+          </p>
+        )}
       </div>
       <button
         type="submit"
